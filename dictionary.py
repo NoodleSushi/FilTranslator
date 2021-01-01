1cd79af86295f7ba7a4facf3a75defcbbe8435ee
Mike Trevor <ytmikesstuffs@gmail.com>
2021-01-02 01:05:22 +0800

    Minor dictionary.py rewrite

--- dictionary.py
+++ dictionary.py
@@ -1,16 +1,12 @@
 import requests
 import html
-import urllib
+import urllib.parse
 import json
 
 DICT_LINK = "https://www.tagaloglessons.com/ajax/reference_guide_search_results.php?keyword="
 
 
 def translate(word: str = "") -> list:
-    JSONstr = requests.get(DICT_LINK+urllib.parse.quote(word)).text
-    if JSONstr == "[]":
-        return []
-    JSONstr = html.unescape(JSONstr)
-    JSONstr = JSONstr.replace("***", "")
-    JSONstr = JSONstr.replace("^^^", "")
-    return json.loads(JSONstr)
+    json_str: str = requests.get(DICT_LINK+urllib.parse.quote(word)).text
+    json_str = html.unescape(json_str).replace("***", "").replace("^^^", "")
+    return json.loads(json_str)
