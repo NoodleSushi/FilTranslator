215a379d173d42e7fea0ae2ef3ac3a69df02b62d
Mike Trevor <ytmikesstuffs@gmail.com>
2021-01-08 20:06:22 +0800

    Create Dictionary class in dictionary.py

--- dictionary.py
+++ dictionary.py
@@ -3,10 +3,12 @@ import html
 import urllib.parse
 import json
 
-DICT_LINK = "https://www.tagaloglessons.com/ajax/reference_guide_search_results.php?keyword="
 
-
-def translate(word: str = "") -> list:
-    json_str: str = requests.get(DICT_LINK+urllib.parse.quote(word)).text
-    json_str = html.unescape(json_str).replace("***", "").replace("^^^", "")
-    return json.loads(json_str)
+class Dictionary:
+    DICT_LINK = "https://www.tagaloglessons.com/ajax/reference_guide_search_results.php?keyword="
+    
+    @staticmethod
+    def translate(word: str = "") -> list:
+        json_str: str = requests.get(Dictionary.DICT_LINK+urllib.parse.quote(word)).text
+        json_str = html.unescape(json_str).replace("***", "").replace("^^^", "")
+        return json.loads(json_str)
